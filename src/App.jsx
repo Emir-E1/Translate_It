be4011ee6d2777166ff66d98eda79a81ff5ed07f
emir <e1lusion@hotmail.com>
2026-01-21 22:06:31 +0100

    app dev first

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const a = 2;
+
+  return (
+    <>
+      <h1>{count - 22}</h1>
+
+      <h2 className="text-clip text-2xl text-red-700">s sssdd</h2>
+    </>
+  );
+}
+
+export default App;
